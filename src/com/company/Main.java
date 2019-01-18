cc59c753fa06c54016b4b39b820f8941c02b3725
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-18 14:49:36 -0500

    Making an initial commit based off the code I've started with.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to this Text-based Adventure Game.\n");
+        System.out.println("You and your friends are hanging out after school.  You all are in the forest near your " +
+                "high school's football field.  All of a sudden you all hear a loud boom that shakes the ground " +
+                "coming from the direction of the football field and you also hear the town air raid sirens go off.  " +
+                "You and your friends are startled and you all aren't sure what you want to do.  What do you want to " +
+                "do?  ENTER THE NUMBER ONLY");
+
+    }
+}
